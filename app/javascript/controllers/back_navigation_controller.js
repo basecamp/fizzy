8a229ce83574b92f2e373193cafb6f547e11ce9b
Jose Farias <jose@37signals.com>
2024-11-01 17:00:52 -0600

    Use restoration index for back-navigation-controller
    Scenario: you navigate to a bubble, go somewhere else, then click the back button. The history length is greater than 1 but you still can't go back. So we check the restorationIndex instead.

--- app/javascript/controllers/back_navigation_controller.js
+++ app/javascript/controllers/back_navigation_controller.js
@@ -4,7 +4,7 @@ export default class extends Controller {
   static values = { fallbackDestination: String }
 
   connect() {
-    if (history.length > 1) {
+    if (history.state.turbo?.restorationIndex > 0) {
       this.element.href = "javascript:history.back()"
     } else {
       this.element.href = this.fallbackDestinationValue
