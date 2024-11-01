c373dc4bd06726ea3b5f0b09f6fa23955147d03c
Jose Farias <jose@37signals.com>
2024-10-31 18:03:09 -0600

    Revert "Unnecessary referer check"
    This reverts commit 2a5c672df7db522cdfa2a60a7e331255c988ff28.

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,6 +1,6 @@
 module LinksHelper
   def link_back_or_to(path, **, &)
-    path = :back if request.referer.present?
+    path = :back if request.referer.present? && request.referer != request.path
     link_to path, **, &
   end
 end
