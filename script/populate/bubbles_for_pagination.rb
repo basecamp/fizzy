113269924951293049c496622c0b0e7aadd09597
Jorge Manrubia <jorge@37signals.com>
2025-04-07 14:25:10 +0200

    Add script to populate cards in mass

---
+++ script/populate/bubbles_for_pagination.rb
@@ -0,0 +1,29 @@
+require_relative "../../config/environment"
+
+BUBBLES_COUNT = 100
+
+ApplicationRecord.current_tenant = "development-tenant"
+account = Account.first
+user = account.users.first
+Current.session = user.sessions.last
+bucket = account.buckets.first
+
+# Doing
+
+BUBBLES_COUNT.times do |index|
+  bubble = bucket.bubbles.create!(title: "Doing card #{index}", creator: user, status: :published)
+  bubble.engage
+end
+
+# Considering
+
+BUBBLES_COUNT.times do |index|
+  bubble = bucket.bubbles.create!(title: "Considering card #{index}", creator: user, status: :published)
+  bubble.reconsider
+end
+
+# Completed
+BUBBLES_COUNT.times do |index|
+  bubble = bucket.bubbles.create!(title: "Popped card #{index}", creator: user, status: :published)
+  bubble.pop!
+end
