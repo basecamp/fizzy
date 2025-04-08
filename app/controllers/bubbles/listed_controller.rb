92857a3e8be1b1ff172f156852f213581fcec14a
Jorge Manrubia <jorge@37signals.com>
2025-04-08 11:50:19 +0200

    Remove trace

--- app/controllers/bubbles/listed_controller.rb
+++ app/controllers/bubbles/listed_controller.rb
@@ -6,11 +6,6 @@ class Bubbles::ListedController < ApplicationController
   before_action :set_filter, only: :index
 
   def index
-
-    Rails.logger.info "-" *100
-    Rails.logger.info @filter.bubbles.popped.count
-    Rails.logger.info "-" *100
-
     set_page_and_extract_portion_from @filter.bubbles.load_async
   end
 
