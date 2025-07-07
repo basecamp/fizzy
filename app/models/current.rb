7f1bdce0b3cf9545acf3634d9fabb53e6dce86f3
Mike Dalessio <mike@37signals.com>
2025-07-07 13:51:14 -0400

    Implement Current.signal_user=
    and split out Current.user as a separate property so that we can set
    it for these one-time rich link tokens without creating a session.

--- app/models/current.rb
+++ app/models/current.rb
@@ -1,7 +1,14 @@
 class Current < ActiveSupport::CurrentAttributes
-  attribute :session
+  attribute :session, :user
   attribute :http_method, :request_id, :user_agent, :ip_address, :referrer
   attribute :account
 
-  delegate :user, to: :session, allow_nil: true
+  def session=(session)
+    super
+    Current.user = session&.user
+  end
+
+  def signal_user=(signal_user)
+    self.user = signal_user&.peer
+  end
 end
