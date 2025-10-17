c648e3a895f0acfb583edcebf5287dff95732078
Stanko K.R. <stanko@stanko.io>
2025-10-17 14:53:56 +0200

    Remove setup_status
    Now that the flow is slightly different we don't need it anymore

--- app/controllers/sessions/login_menus_controller.rb
+++ app/controllers/sessions/login_menus_controller.rb
@@ -8,13 +8,14 @@ class Sessions::LoginMenusController < ApplicationController
   end
 
   def create
-    user = User.find_by(email_address: IdentityProvider.resolve_token(resume_identity))
+    email_address = IdentityProvider.resolve_token(resume_identity)
+    user = User.find_by(email_address: email_address)
 
     if user
       start_new_session_for(user)
       redirect_to after_authentication_url
     else
-      IdentityProvider.unlink(email_address: IdentityProvider.resolve_token(Current.identity_token), from: ApplicationRecord.current_tenant)
+      IdentityProvider.unlink(email_address: email_address, from: ApplicationRecord.current_tenant)
       redirect_to session_login_menu_path, alert: "You can't access this account"
     end
   end
