f34dcf5084e823e75f8e12de4ee6173ec7008476
Stanko K.R. <stanko@stanko.io>
2025-10-19 18:25:14 +0200

    Scope down the rescue to StandardError

--- app/models/user/invitable.rb
+++ app/models/user/invitable.rb
@@ -6,9 +6,9 @@ module User::Invitable
       create!(attributes).tap do |user|
         IdentityProvider.link(email_address: user.email_address, to: ApplicationRecord.current_tenant)
         IdentityProvider.send_magic_link(user.email_address)
-      rescue
+      rescue => e
         user.destroy!
-        raise
+        raise e
       end
     end
   end
