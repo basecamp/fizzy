dacfeef7e8066305f287e033465c426b19b82852
Stanko K.R. <stanko@stanko.io>
2025-10-19 12:43:20 +0200

    Remove unused method

--- app/models/identity_provider.rb
+++ app/models/identity_provider.rb
@@ -12,8 +12,4 @@ module IdentityProvider
   end
 
   delegate :link, :unlink, :send_magic_link, :consume_magic_link, :tenants_for, :token_for, :resolve_token, :verify_token, to: :backend
-
-  def tenants_for(identity_token)
-    backend.tenants_for(identity_token)
-  end
 end
