2c7a71bcea3dd385767a39736e9d43002fea915d
Donal McBreen <donal@37signals.com>
2025-11-19 12:02:26 +0000

    Add SQLite support
    - UUID support
    - Schema compatibility layer, ignore MySQL specific options
    - Search not working yet

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -6,5 +6,10 @@ end
 
 # Use DB read/write splitting for Active Storage models
 ActiveSupport.on_load(:active_storage_record) do
-  connects_to database: { writing: :primary, reading: :replica }
+  # SQLite doesn't use separate replica databases
+  if ENV.fetch("DATABASE_ADAPTER", "mysql") == "sqlite"
+    connects_to database: { writing: :primary, reading: :primary }
+  else
+    connects_to database: { writing: :primary, reading: :replica }
+  end
 end
