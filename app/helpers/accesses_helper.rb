cae62e4d84486a5c36fb62458143ced6382eca3d
David Heinemeier Hansson <david@hey.com>
2025-12-04 15:31:39 -0800

    Correct it and clean it

--- app/helpers/accesses_helper.rb
+++ app/helpers/accesses_helper.rb
@@ -30,34 +30,23 @@ module AccessesHelper
     displayed_watchers = watchers.first(8)
     overflow_count = watchers.size - 8
 
-    tag.strong(displayed_watchers.any? ? "Watching for new cards" : "No one is watching for new cards", class: "txt-uppercase") do
-      tag.div(avatar_tags(displayed_watchers), class: "board-tools__watching") do
-        tag.div(data: { controller: "dialog", action: "keydown.esc->dialog#close click@document->dialog#closeOnClickOutside" }) do
-          tag.button("+#{overflow_count}", class: "overflow-count btn btn--circle borderless", data: { action: "dialog#open" }, aria: { label: "Show #{overflow_count} more watchers" }) +
-          tag.dialog(avatar_tags(watchers), class: "board-tools__watching-dialog dialog panel", data: { dialog_target: "dialog" }, aria: { hidden: "true" })
-        end if overflow_count > 0
-      end
+    tag.strong(displayed_watchers.any? ? "Watching for new cards" : "No one is watching for new cards", class: "txt-uppercase") +
+    tag.div(avatar_tags(displayed_watchers), class: "board-tools__watching") do
+      tag.div(data: { controller: "dialog", action: "keydown.esc->dialog#close click@document->dialog#closeOnClickOutside" }) do
+        tag.button("+#{overflow_count}", class: "overflow-count btn btn--circle borderless", data: { action: "dialog#open" }, aria: { label: "Show #{overflow_count} more watchers" }) +
+        tag.dialog(avatar_tags(watchers), class: "board-tools__watching-dialog dialog panel", data: { dialog_target: "dialog" }, aria: { hidden: "true" })
+      end if overflow_count > 0
     end
   end
 
   def involvement_button(board, access, show_watchers, icon_only)
-    involvement_label_id = dom_id(board, :involvement_label)
-
     button_to(
-      board_involvement_path(board),
-      method: :put,
-      aria: { labelledby: involvement_label_id },
-      title: involvement_access_label(access),
-      class: class_names("btn", { "btn--reversed": access.watching? && icon_only }),
-      params: { show_watchers: show_watchers, involvement: next_involvement(access.involvement), icon_only: icon_only }
-    ) do
-      safe_join([
-        icon_tag("notification-bell-#{icon_only ? 'reverse-' : nil}#{access.involvement.dasherize}"),
-        tag.span(
-          class: class_names("txt-nowrap txt-uppercase", "for-screen-reader": icon_only),
-          id: involvement_label_id
-        )
-      ])
+      board_involvement_path(board), method: :put,
+      params: { show_watchers: show_watchers, involvement: next_involvement(access.involvement), icon_only: icon_only },
+      aria: { labelledby: dom_id(board, :involvement_label) }, title: access.access_only? ? "Watch this" : "Stop watching",
+      class: class_names("btn", { "btn--reversed": access.watching? && icon_only })) do
+        icon_tag("notification-bell-#{icon_only ? 'reverse-' : nil}#{access.involvement.dasherize}") +
+        tag.span(class: class_names("txt-nowrap txt-uppercase", "for-screen-reader": icon_only), id: dom_id(board, :involvement_label))
     end
   end
 
@@ -65,13 +54,5 @@ module AccessesHelper
     def next_involvement(involvement)
       order = %w[ watching access_only ]
       order[(order.index(involvement.to_s) + 1) % order.size]
-    end
-
-    def involvement_access_label(access)
-      if access.access_only?
-        "Watch this"
-      else
-        "Stop watching"
-      end
     end
 end
