1f636375ba306d06f93db486a1f01824676d4410
Felipe Felix <felippe.felix98@gmail.com>
2025-12-25 17:57:55 -0300

    suppressing_turbo_broadcasts

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,11 @@
+# rbs_inline: enabled
+
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
   configure_replica_connections
+
+  # @rbs!
+  #   def self.configure_replica_connections: () -> void
+  #   def self.suppressing_turbo_broadcasts: () { () -> untyped } -> void
 end
