c24b5afbf49269baf285dacdef2c7d935be79224
Mike Dalessio <mike@37signals.com>
2026-02-07 13:53:51 -0500

    Group notification emails by board
    Notification bundle emails now group items by board instead of
    showing the board name redundantly for each card. Each board
    section has a linked header and an <hr> separator.

--- test/mailers/notification/bundle_mailer_test.rb
+++ test/mailers/notification/bundle_mailer_test.rb
@@ -37,8 +37,55 @@ class Notification::BundleMailerTest < ActionMailer::TestCase
     assert_match /alt="#{@user.name}"/, email.html_part.body.to_s
   end
 
+  test "groups notifications by board" do
+    private_board = boards(:private)
+    private_card = Current.with(user: @user) do
+      private_board.cards.create!(
+        title: "Private card", creator: @user, status: :published, account: @user.account
+      )
+    end
+    private_event = Event.create!(
+      creator: @user, board: private_board, eventable: private_card,
+      action: :card_published, account: @user.account
+    )
+
+    create_notification(@user, source: events(:logo_published))
+    create_notification(@user, source: private_event)
+    create_notification(@user, source: events(:layout_published))
+
+    html = Notification::BundleMailer.notification(@bundle).html_part.body.to_s
+
+    board_headers = html.scan(/class="notification__board"/)
+    assert_equal 2, board_headers.size, "Should have exactly two board headers"
+
+    assert_match(/Writebook/, html)
+    assert_match(/Private board/, html)
+  end
+
+  test "board header links to the board" do
+    create_notification(@user, source: events(:logo_published))
+
+    html = Notification::BundleMailer.notification(@bundle).html_part.body.to_s
+    board = boards(:writebook)
+
+    assert_match %r{<a[^>]*href="[^"]*boards/#{board.id}"[^>]*>Writebook</a>}, html
+  end
+
+  test "shows multiple cards under same board header" do
+    create_notification(@user, source: events(:logo_published))
+    create_notification(@user, source: events(:layout_published))
+
+    html = Notification::BundleMailer.notification(@bundle).html_part.body.to_s
+
+    board_header_count = html.scan(/class="notification__board"/).size
+    assert_equal 1, board_header_count, "Same board should only have one header"
+
+    assert_match(/The logo isn/, html)
+    assert_match(/Layout is broken/, html)
+  end
+
   private
-    def create_notification(user)
-      Notification.create!(user: user, creator: user, source: events(:logo_published), created_at: 30.minutes.ago)
+    def create_notification(user, source: events(:logo_published))
+      Notification.create!(user: user, creator: user, source: source, created_at: 30.minutes.ago)
     end
 end
