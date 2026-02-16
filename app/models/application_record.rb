8effba3807c103b481175b3e5e0ec6af6b11564c
Felipe Felix <felippe.felix98@gmail.com>
2026-02-16 13:35:08 -0300

    adjust broadcasts args

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -8,13 +8,13 @@ class ApplicationRecord < ActiveRecord::Base
   # @rbs!
   #   def self.configure_replica_connections: () -> void
   #   def self.suppressing_turbo_broadcasts: () { () -> untyped } -> void
-  #   def self.broadcasts_refreshes: (**untyped) -> void
-  #   def self.broadcasts_refreshes_to: (untyped, **untyped) -> void
+  #   def self.broadcasts_refreshes: (*untyped) -> void
+  #   def self.broadcasts_refreshes_to: (*untyped) -> void
   #
-  #   def broadcast_replace_later_to: (untyped, **untyped) -> void
-  #   def broadcast_prepend_to: (untyped, **untyped) -> void
-  #   def broadcast_remove_to: (untyped, **untyped) -> void
-  #   def broadcast_prepend_later_to: (untyped, **untyped) -> void
+  #   def broadcast_replace_later_to: (*untyped) -> void
+  #   def broadcast_prepend_to: (*untyped) -> void
+  #   def broadcast_remove_to: (*untyped) -> void
+  #   def broadcast_prepend_later_to: (*untyped) -> void
   #
   #   def self.due_to_be_postponed: () -> void
 end
