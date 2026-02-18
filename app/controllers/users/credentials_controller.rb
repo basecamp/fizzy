4e4db59aadf03f2c50872c2a466a22897503fce0
Stanko K.R. <stanko@stanko.io>
2026-02-18 15:49:21 +0100

    Fix typos

--- app/controllers/users/credentials_controller.rb
+++ app/controllers/users/credentials_controller.rb
@@ -1,44 +1,40 @@
 class Users::CredentialsController < ApplicationController
-  before_action :set_user
+  before_action :set_credential, only: :destroy
 
   def index
-    @credentials = identity.credentials.order(created_at: :desc)
+    @credentials = Current.identity.credentials.order(name: :asc, created_at: :desc)
   end
 
   def new
-    @creation_options = Identity::Credential.creation_options(identity: identity, display_name: @user.name)
+    @creation_options = Identity::Credential.creation_options(identity: Current.identity, display_name: Current.user.name)
     session[:webauthn_challenge] = @creation_options.challenge
   end
 
   def create
     Identity::Credential.register(
-      identity: identity,
-      name: params.dig(:credential, :name),
-      client_data_json: credential_response[:client_data_json],
-      attestation_object: credential_response[:attestation_object],
+      identity: Current.identity,
+      name: credential_params[:name],
+      client_data_json: credential_params.dig(:response, :client_data_json),
+      attestation_object: credential_params.dig(:response, :attestation_object),
       challenge: session.delete(:webauthn_challenge),
       origin: request.base_url,
-      transports: Array(credential_response[:transports])
+      transports: Array(credential_params.dig(:response, :transports))
     )
 
-    redirect_to user_credentials_path(@user)
+    redirect_to user_credentials_path(Current.user)
   end
 
   def destroy
-    identity.credentials.find(params[:id]).destroy!
-    redirect_to user_credentials_path(@user)
+    @credential.destroy!
+    redirect_to user_credentials_path(Current.user)
   end
 
   private
-    def set_user
-      @user = Current.identity.users.find(params[:user_id])
+    def set_credential
+      @credential = Current.identity.credentials.find(params[:id])
     end
 
-    def identity
-      @user.identity
-    end
-
-    def credential_response
-      params.expect(credential: { response: [ :client_data_json, :attestation_object, transports: [] ] })[:response]
+    def credential_params
+      params.expect(credential: [ :name, response: [ :client_data_json, :attestation_object, transports: [] ] ])
     end
 end
