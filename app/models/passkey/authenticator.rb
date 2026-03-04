81cf29546528f5013f92ed629ed2e6a73719d0b8
Stanko K.R. <stanko@stanko.io>
2026-03-04 14:44:43 +0100

    Simplify authenticator registry loading

--- app/models/passkey/authenticator.rb
+++ app/models/passkey/authenticator.rb
@@ -1,12 +1,23 @@
-class Passkey::Authenticator < Data.define(:name, :icon)
-  REGISTRY = Rails.application.config_for(:passkey_aaguids).each_with_object({}) do |(_key, attrs), hash|
-    authenticator = new(name: attrs[:name], icon: attrs[:icon])
-    attrs[:aaguids].each { |aaguid| hash[aaguid] = authenticator }
-  end.freeze
-
+class Passkey::Authenticator < Data.define(:aaguids, :name, :icon)
   class << self
     def find_by_aaguid(aaguid)
-      REGISTRY[aaguid]
+      registry[aaguid]
+    end
+
+    def registry
+      @registry ||= Hash.new.tap do |registry|
+        all.each do |authenticator|
+          authenticator.aaguids.each do |aaguid|
+            registry[aaguid] = authenticator
+          end
+        end
+      end
+    end
+
+    def all
+      Rails.application.config_for(:passkey_aaguids).each_value.map do |attrs|
+        new(aaguids: attrs[:aaguids], name: attrs[:name], icon: attrs[:icon])
+      end
     end
   end
 end
