d006d6eda0a9832905d2a5f8058dfe65727ef89b
Stanko K.R. <stanko@stanko.io>
2026-03-05 18:57:49 +0100

    Pass params hashes down to ActionPack

--- lib/action_pack/web_authn/public_key_credential.rb
+++ lib/action_pack/web_authn/public_key_credential.rb
@@ -1,11 +1,23 @@
 class ActionPack::WebAuthn::PublicKeyCredential
-  attr_reader :id, :public_key, :sign_count, :aaguid, :backed_up, :transports, :owner
+  attr_reader :id, :public_key, :sign_count, :aaguid, :backed_up, :transports
 
   class << self
-    def create(client_data_json:, attestation_object:, challenge:, origin:, transports: [], owner: nil)
+    def request_options(**attributes)
+      attributes[:exclude_credentials] = transform_credentials(attributes[:exclude_credentials]) if attributes[:exclude_credentials]
+
+      ActionPack::WebAuthn::PublicKeyCredential::RequestOptions.new(**attributes)
+    end
+
+    def creation_options(**attributes)
+      attributes[:credentials] = transform_credentials(attributes[:credentials]) if attributes[:credentials]
+
+      ActionPack::WebAuthn::PublicKeyCredential::CreationOptions.new(**attributes)
+    end
+
+    def register(params, challenge:, origin: ActionPack::WebAuthn::Current.origin)
       response = ActionPack::WebAuthn::Authenticator::AttestationResponse.new(
-        client_data_json: client_data_json,
-        attestation_object: attestation_object,
+        client_data_json: params[:client_data_json],
+        attestation_object: Base64.urlsafe_decode64(params[:attestation_object]),
         challenge: challenge,
         origin: origin
       )
@@ -18,27 +30,37 @@ class ActionPack::WebAuthn::PublicKeyCredential
         sign_count: response.attestation.sign_count,
         aaguid: response.attestation.aaguid,
         backed_up: response.attestation.backed_up?,
-        transports: transports,
-        owner: owner
+        transports: Array(params[:transports])
       )
     end
+
+    private
+      def transform_credentials(credentials)
+        Array(credentials).map do |credential|
+          if credential.respond_to?(:to_public_key_credential)
+            credential.to_public_key_credential
+          else
+            credential
+          end
+        end
+      end
   end
 
-  def initialize(id:, public_key:, sign_count:, aaguid: nil, backed_up: nil, transports: [], owner: nil)
+  def initialize(id:, public_key:, sign_count:, aaguid: nil, backed_up: nil, transports: [])
     @id = id
     @public_key = public_key
+    @public_key = OpenSSL::PKey.read(public_key) unless public_key.is_a?(OpenSSL::PKey::PKey)
     @sign_count = sign_count
     @aaguid = aaguid
     @backed_up = backed_up
     @transports = transports
-    @owner = owner
   end
 
-  def authenticate(client_data_json:, authenticator_data:, signature:, challenge:, origin:)
+  def authenticate(params, challenge:, origin: ActionPack::WebAuthn::Current.origin)
     response = ActionPack::WebAuthn::Authenticator::AssertionResponse.new(
-      client_data_json: client_data_json,
-      authenticator_data: authenticator_data,
-      signature: signature,
+      client_data_json: params[:client_data_json],
+      authenticator_data: Base64.urlsafe_decode64(params[:authenticator_data]),
+      signature: Base64.urlsafe_decode64(params[:signature]),
       credential: self,
       challenge: challenge,
       origin: origin
@@ -48,5 +70,16 @@ class ActionPack::WebAuthn::PublicKeyCredential
 
     @sign_count = response.authenticator_data.sign_count
     @backed_up = response.authenticator_data.backed_up?
+  end
+
+  def to_h
+    {
+      credential_id: id,
+      public_key: public_key.to_der,
+      sign_count: sign_count,
+      aaguid: aaguid,
+      backed_up: backed_up,
+      transports: transports
+    }
   end
 end
