fa1981b7245475816ca008100db7a63f36c029d5
Stanko K.R. <stanko@stanko.io>
2026-03-11 15:30:47 +0100

    Simplify railtie

--- lib/action_pack/railtie.rb
+++ lib/action_pack/railtie.rb
@@ -1,14 +1,15 @@
-require_relative "../web_authn"
+require_relative "web_authn"
 
-class ActionPack::WebAuthn::Railtie < Rails::Railtie
+class ActionPack::Railtie < Rails::Railtie
   config.action_pack = ActiveSupport::OrderedOptions.new unless config.respond_to?(:action_pack)
+
   config.action_pack.web_authn = ActiveSupport::OrderedOptions.new
   config.action_pack.web_authn.default_request_options = {}
   config.action_pack.web_authn.default_creation_options = {}
 
   initializer "action_pack.passkey.form_helper" do
     ActiveSupport.on_load(:action_view) do
-      require_relative "../passkey/form_helper"
+      require_relative "passkey/form_helper"
       include ActionPack::Passkey::FormHelper
     end
   end
