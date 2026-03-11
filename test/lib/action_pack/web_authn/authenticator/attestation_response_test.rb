39d915e51d5d6d718b9ca997bae5192b901e70a3
Stanko K.R. <stanko@stanko.io>
2026-03-11 18:59:03 +0100

    Replace in-place CBOR encoder with pre-generated known-good examples

--- test/lib/action_pack/web_authn/authenticator/attestation_response_test.rb
+++ test/lib/action_pack/web_authn/authenticator/attestation_response_test.rb
@@ -1,6 +1,39 @@
 require "test_helper"
 
 class ActionPack::WebAuthn::Authenticator::AttestationResponseTest < ActiveSupport::TestCase
+  # All CBOR data generated with Python cbor2 v5.8.0
+  #
+  # Auth data in all objects contains:
+  #   rp_id_hash: SHA-256("example.com") (32 bytes)
+  #   sign_count: 0
+  #   aaguid: 00010203-0405-0607-0809-0a0b0c0d0e0f (16 bytes)
+  #   credential_id: 32 sequential bytes 0x00..0x1f
+  #   cose_key: EC2/ES256 P-256 {1: 2, 3: -7, -1: 1, -2: <x>, -3: <y>}
+
+  # {"fmt": "none", "attStmt": {}, "authData": <flags: 0x45 (UP+UV+AT)>}
+  ATTESTATION_NONE_VERIFIED = ["a363666d74646e6f6e656761747453746d74a068617574684461" \
+    "746158a4a379a6f6eeafb9a55e378c118034e2751e682fab9f2d30ab13d2125586ce1947" \
+    "4500000000000102030405060708090a0b0c0d0e0f0020000102030405060708090a0b0c" \
+    "0d0e0f101112131415161718191a1b1c1d1e1fa50102032620012158202ba472104c686f" \
+    "39d4b623cc9324953e7053b47cae818e8cf774203a4f51af7122582069cb8ac519bdd929" \
+    "e2bdbe79e9f9b8d14c2d89a7cbd324647a1ccd68b8de3ca0"].pack("H*")
+
+  # {"fmt": "none", "attStmt": {}, "authData": <flags: 0x41 (UP+AT)>}
+  ATTESTATION_NONE_NOT_VERIFIED = ["a363666d74646e6f6e656761747453746d74a06861757468" \
+    "4461746158a4a379a6f6eeafb9a55e378c118034e2751e682fab9f2d30ab13d2125586ce" \
+    "19474100000000000102030405060708090a0b0c0d0e0f0020000102030405060708090a" \
+    "0b0c0d0e0f101112131415161718191a1b1c1d1e1fa50102032620012158202ba472104c" \
+    "686f39d4b623cc9324953e7053b47cae818e8cf774203a4f51af7122582069cb8ac519bd" \
+    "d929e2bdbe79e9f9b8d14c2d89a7cbd324647a1ccd68b8de3ca0"].pack("H*")
+
+  # {"fmt": "packed", "attStmt": {}, "authData": <flags: 0x45 (UP+UV+AT)>}
+  ATTESTATION_PACKED_VERIFIED = ["a363666d74667061636b65646761747453746d74a068617574" \
+    "684461746158a4a379a6f6eeafb9a55e378c118034e2751e682fab9f2d30ab13d2125586" \
+    "ce19474500000000000102030405060708090a0b0c0d0e0f0020000102030405060708090" \
+    "a0b0c0d0e0f101112131415161718191a1b1c1d1e1fa50102032620012158202ba472104" \
+    "c686f39d4b623cc9324953e7053b47cae818e8cf774203a4f51af7122582069cb8ac519b" \
+    "dd929e2bdbe79e9f9b8d14c2d89a7cbd324647a1ccd68b8de3ca0"].pack("H*")
+
   setup do
     ActionPack::WebAuthn::Current.host = "example.com"
 
@@ -14,7 +47,7 @@ class ActionPack::WebAuthn::Authenticator::AttestationResponseTest < ActiveSuppo
 
     @response = ActionPack::WebAuthn::Authenticator::AttestationResponse.new(
       client_data_json: @client_data_json,
-      attestation_object: build_attestation_object(user_verified: true),
+      attestation_object: ATTESTATION_NONE_VERIFIED,
       challenge: @challenge,
       origin: @origin
     )
@@ -33,7 +66,7 @@ class ActionPack::WebAuthn::Authenticator::AttestationResponseTest < ActiveSuppo
   test "validate! succeeds with user_verification preferred when not verified" do
     response = ActionPack::WebAuthn::Authenticator::AttestationResponse.new(
       client_data_json: @client_data_json,
-      attestation_object: build_attestation_object(user_verified: false),
+      attestation_object: ATTESTATION_NONE_NOT_VERIFIED,
       challenge: @challenge,
       origin: @origin,
       user_verification: :preferred
@@ -47,7 +80,7 @@ class ActionPack::WebAuthn::Authenticator::AttestationResponseTest < ActiveSuppo
   test "validate! succeeds with user_verification required when verified" do
     response = ActionPack::WebAuthn::Authenticator::AttestationResponse.new(
       client_data_json: @client_data_json,
-      attestation_object: build_attestation_object(user_verified: true),
+      attestation_object: ATTESTATION_NONE_VERIFIED,
       challenge: @challenge,
       origin: @origin,
       user_verification: :required
@@ -61,7 +94,7 @@ class ActionPack::WebAuthn::Authenticator::AttestationResponseTest < ActiveSuppo
   test "validate! raises with user_verification required when not verified" do
     response = ActionPack::WebAuthn::Authenticator::AttestationResponse.new(
       client_data_json: @client_data_json,
-      attestation_object: build_attestation_object(user_verified: false),
+      attestation_object: ATTESTATION_NONE_NOT_VERIFIED,
       challenge: @challenge,
       origin: @origin,
       user_verification: :required
@@ -83,7 +116,7 @@ class ActionPack::WebAuthn::Authenticator::AttestationResponseTest < ActiveSuppo
 
     response = ActionPack::WebAuthn::Authenticator::AttestationResponse.new(
       client_data_json: client_data_json,
-      attestation_object: build_attestation_object(user_verified: true),
+      attestation_object: ATTESTATION_NONE_VERIFIED,
       challenge: @challenge,
       origin: @origin
     )
@@ -118,7 +151,7 @@ class ActionPack::WebAuthn::Authenticator::AttestationResponseTest < ActiveSuppo
   test "validate! raises when attestation format is not registered" do
     response = ActionPack::WebAuthn::Authenticator::AttestationResponse.new(
       client_data_json: @client_data_json,
-      attestation_object: build_attestation_object(user_verified: true, format: "packed"),
+      attestation_object: ATTESTATION_PACKED_VERIFIED,
       challenge: @challenge,
       origin: @origin
     )
@@ -139,7 +172,7 @@ class ActionPack::WebAuthn::Authenticator::AttestationResponseTest < ActiveSuppo
 
     response = ActionPack::WebAuthn::Authenticator::AttestationResponse.new(
       client_data_json: @client_data_json,
-      attestation_object: build_attestation_object(user_verified: true, format: "packed"),
+      attestation_object: ATTESTATION_PACKED_VERIFIED,
       challenge: @challenge,
       origin: @origin
     )
@@ -150,85 +183,4 @@ class ActionPack::WebAuthn::Authenticator::AttestationResponseTest < ActiveSuppo
     ActionPack::WebAuthn.attestation_verifiers.delete("packed")
   end
 
-  private
-    def build_attestation_object(user_verified:, format: "none")
-      auth_data = build_authenticator_data(user_verified: user_verified)
-      encode_cbor_attestation_object(auth_data, format: format)
-    end
-
-    def build_authenticator_data(user_verified:)
-      rp_id_hash = Digest::SHA256.digest("example.com")
-      flags = 0x41 # user present + attested credential
-      flags |= 0x04 if user_verified
-      sign_count = 0
-      aaguid = SecureRandom.random_bytes(16)
-      credential_id = SecureRandom.random_bytes(32)
-      cose_key = build_cose_key
-
-      bytes = []
-      bytes.concat(rp_id_hash.bytes)
-      bytes << flags
-      bytes.concat([ sign_count ].pack("N").bytes)
-      bytes.concat(aaguid.bytes)
-      bytes.concat([ credential_id.bytesize ].pack("n").bytes)
-      bytes.concat(credential_id.bytes)
-      bytes.concat(cose_key.bytes)
-      bytes.pack("C*")
-    end
-
-    def build_cose_key
-      ec_key = OpenSSL::PKey::EC.generate("prime256v1")
-      public_key_bn = ec_key.public_key.to_bn
-      public_key_point = public_key_bn.to_s(2)
-      x = public_key_point[1, 32]
-      y = public_key_point[33, 32]
-
-      params = { 1 => 2, 3 => -7, -1 => 1, -2 => x, -3 => y }
-      encode_cbor_map(params)
-    end
-
-    def encode_cbor_attestation_object(auth_data, format:)
-      bytes = [ 0xa3 ]
-      bytes.concat([ 0x63, *"fmt".bytes ])
-      bytes.concat([ 0x40 + format.bytesize, *format.bytes ])
-      bytes.concat([ 0x67, *"attStmt".bytes ])
-      bytes << 0xa0
-      bytes.concat([ 0x68, *"authData".bytes ])
-      if auth_data.bytesize <= 255
-        bytes.concat([ 0x58, auth_data.bytesize ])
-      else
-        bytes.concat([ 0x59, (auth_data.bytesize >> 8) & 0xff, auth_data.bytesize & 0xff ])
-      end
-      bytes.concat(auth_data.bytes)
-      bytes.pack("C*")
-    end
-
-    def encode_cbor_map(hash)
-      bytes = [ 0xa0 + hash.size ]
-      hash.each do |key, value|
-        bytes.concat(encode_cbor_integer(key))
-        bytes.concat(encode_cbor_value(value))
-      end
-      bytes.pack("C*")
-    end
-
-    def encode_cbor_integer(int)
-      if int >= 0 && int <= 23
-        [ int ]
-      elsif int >= -24 && int < 0
-        [ 0x20 - int - 1 ]
-      else
-        raise "Integer encoding not implemented for #{int}"
-      end
-    end
-
-    def encode_cbor_value(value)
-      case value
-      when Integer
-        encode_cbor_integer(value)
-      when String
-        length = value.bytesize
-        length <= 23 ? [ 0x40 + length, *value.bytes ] : [ 0x58, length, *value.bytes ]
-      end
-    end
 end
