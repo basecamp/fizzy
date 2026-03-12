5b8ae61f338e5a032086ff7241367a83c5b1198f
Stanko K.R. <stanko@stanko.io>
2026-03-12 12:57:58 +0100

    Squash Passkey migrations

--- db/migrate/20260213154740_create_identity_credentials.rb
+++ db/migrate/20260213154740_create_identity_credentials.rb
@@ -1,16 +1,19 @@
 class CreateIdentityCredentials < ActiveRecord::Migration[8.2]
   def change
-    create_table :identity_credentials, id: :uuid do |t|
-      t.uuid :identity_id, null: false
+    create_table :action_pack_passkeys, id: :uuid do |t|
+      t.uuid :holder_id, null: false
+      t.string :holder_type, null: false
       t.string :credential_id, null: false
       t.binary :public_key, null: false
       t.integer :sign_count, null: false, default: 0
       t.string :name
       t.text :transports
+      t.string :aaguid
+      t.boolean :backed_up
 
       t.timestamps
 
-      t.index :identity_id
+      t.index [ :holder_type, :holder_id ]
       t.index :credential_id, unique: true
     end
   end
--- db/migrate/20260312065104_rename_passkeys_to_action_pack_passkeys.rb
+++
@@ -1,5 +0,0 @@
-class RenamePasskeysToActionPackPasskeys < ActiveRecord::Migration[8.2]
-  def change
-    rename_table :passkeys, :action_pack_passkeys
-  end
-end
