3d543900c301260f3dd1658c786c6b6976a7019b
Zacharias Dyna Knudsen <z@chari.as>
2026-03-20 14:08:09 +0100

    Use possessive quantifiers in excerpt helper regexps to prevent backtracking
    Also cleans up the format_html helper.

--- app/helpers/excerpt_helper.rb
+++ app/helpers/excerpt_helper.rb
@@ -3,9 +3,9 @@ module ExcerptHelper
     return "" if content.blank?
 
     text = content.respond_to?(:to_plain_text) ? content.to_plain_text : content.to_s
-    text = text.gsub(/^>\s*(.*)$/m, '> \1')
-    text = text.gsub(/^\s*[-+]\s*(.*)$/m, '• \1')
-    text = text.gsub(/^\d+\.\s*(.*)$/m) { |m| m }
+    text = text.gsub(/^>\s*+(.*)$/m, '> \1')
+    text = text.gsub(/^\s*+[-+]\s*+(.*)$/m, '• \1')
+    text = text.gsub(/^\d+\.\s*+(.*)$/m) { |m| m }
     text = text.gsub(/\s+/, " ").strip
     text.truncate(length)
   end
--- app/helpers/html_helper.rb
+++ app/helpers/html_helper.rb
@@ -1,7 +1,6 @@
 module HtmlHelper
   def format_html(html)
-    @auto_link_scrubber ||= AutoLinkScrubber.new
-    Loofah::HTML5::DocumentFragment.parse(html).scrub!(@auto_link_scrubber).to_html.html_safe
+    Loofah::HTML5::DocumentFragment.parse(html).scrub!(auto_link_scrubber).to_html.html_safe
   end
 
   def card_html_title(card)
@@ -9,4 +8,9 @@ module HtmlHelper
 
     ERB::Util.html_escape(card.title).gsub(/`([^`]+)`/, '<code>\1</code>').html_safe
   end
+
+  private
+    def auto_link_scrubber
+      @auto_link_scrubber ||= AutoLinkScrubber.new
+    end
 end
